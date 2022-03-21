e680602cc9ea8c98d6e72f7df12d1eccb30aa9e2
Akash <aakashsinghbkj@gmail.com>
2022-03-21 07:30:41 +0530

    Commiting all

---
+++ app/src/main/java/com/akash/healthyou/solutionChart.java
@@ -0,0 +1,15 @@
+package com.akash.healthyou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class solutionChart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solution_chart);
+        getSupportActionBar().hide();
+    }
+}
